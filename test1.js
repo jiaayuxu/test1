b8ab976af4fb8fc6f3251f7ab478aa9a5291d800
jiaayuxu <jx2@hawaii.edu>
2018-09-20 11:58:45 -1000

    asf

--- test1.js
+++ test1.js
@@ -1,8 +1,32 @@
 
 
-function fibonacciSequence(){
+function fiberoo(limit){
 
-  const variable = 0;
+  let num1;
+  let num2;
+  const result[];
 
+  result.push(num1);
+  result.push(num2);
+
+
+  // 100
+
+  for ( let i =0; i< limit - 2; i++){
+     const next = num1 + num2;
+     num1 = num2;
+     num2 = next;
+
+  }
+   return result;
 
 }
+
+function numToString(data){
+  return _.map(data,n => '${n}');
+}
+
+function numEvenNums (data){
+  return _.filter(data, n => (n%2)===0).length;
+}
+
